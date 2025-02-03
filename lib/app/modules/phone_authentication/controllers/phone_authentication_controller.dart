ac4cb0a185ca78321a53b895d7eef6e0040e32e7
UK-Dev786 <ukdev.codetivelab@gmail.com>
2025-02-03 12:30:29 +0500

    phone-auth
    done

--- lib/app/modules/phone_authentication/controllers/phone_authentication_controller.dart
+++ lib/app/modules/phone_authentication/controllers/phone_authentication_controller.dart
@@ -1,23 +1,57 @@
+import 'dart:developer';
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:luncher/app/routes/app_pages.dart';
+import 'package:luncher/services/authentication_service.dart';
+
+import '../../../../widgets/custom_snackbar.dart';
 
 class PhoneAuthenticationController extends GetxController {
-  //TODO: Implement PhoneAuthenticationController
+  final phoneController = TextEditingController();
+  final isLoading = false.obs;
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  final AuthenticationService _authService = AuthenticationService();
+
+  // Validate phone number
+  bool validatePhoneNumber(String phoneNumber) {
+    final phoneRegex = RegExp(r'^\+?[1-9]\d{9,14}$'); // International format
+    if (phoneNumber.isEmpty) {
+      showCustomSnack("Phone number can't be empty");
+      return false;
+    } else if (!phoneRegex.hasMatch(phoneNumber)) {
+      showCustomSnack("Enter a valid phone number");
+      return false;
+    }
+    return true;
   }
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
+  // Handle phone number authentication
+  Future<void> authenticatePhoneNumber() async {
+    final phoneNumber = phoneController.text.trim();
 
-  @override
-  void onClose() {
-    super.onClose();
-  }
+    log("Phone number: $phoneNumber");
 
-  void increment() => count.value++;
+    if (!validatePhoneNumber(phoneNumber)) return;
+
+    isLoading.value = true;
+
+    // Call the service and wait for the result
+    final result = await _authService.authenticatePhoneNumber(phoneNumber);
+
+    isLoading.value = false;
+
+    // Handle result
+    if (result.success) {
+      if (result.verificationId != null) {
+        // Navigate to OTP verification screen
+        Get.toNamed(Routes.PHONE_VERIFICATION,
+            arguments: result.verificationId);
+      } else {
+        // Navigate to the home screen if no verificationId (auto-verification)
+        Get.offAllNamed(Routes.PARENTS_HOME);
+      }
+    } else {
+      showCustomSnack(result.message);
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:luncher/widgets/custom_dialog_schedule.dart';
 
 import 'app/routes/app_pages.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   Get.put(ScheduleDialogController());
   runApp(
     GetMaterialApp(
