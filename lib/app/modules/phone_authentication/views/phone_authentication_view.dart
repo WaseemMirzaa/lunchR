0232cf672620871a263278657c32ccbc283d00b2
UK-Dev786 <ukdev.codetivelab@gmail.com>
2025-03-03 13:22:08 +0500

    phone text field
    Phone text field changes with country code done.

--- lib/app/modules/phone_authentication/views/phone_authentication_view.dart
+++ lib/app/modules/phone_authentication/views/phone_authentication_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
 import 'package:luncher/config/app_colors.dart';
 import 'package:luncher/config/app_text_style.dart';
 import 'package:luncher/widgets/custom_textfeild.dart';
@@ -45,12 +46,41 @@ class PhoneAuthenticationView extends GetView<PhoneAuthenticationController> {
               const SizedBox(height: 20),
 
               // Phone number field
-              TextFieldWidget(
-                text: 'Mobile No',
-                path: 'assets/icon/call_light.png',
-                keyboardType: TextInputType.phone,
-                textController: controller.phoneController,
+              Container(
+                height: 56,
+                width: double.infinity,
+                padding: const EdgeInsets.only(left: 30, right: 10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: AppColors.whiteColor,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      blurRadius: 6,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: IntlPhoneField(
+                  decoration: InputDecoration(
+                    labelText: 'Mobile No',
+                    labelStyle: AppTextStyles.MetropolisRegular.copyWith(
+                      color: const Color(0xFFB6B7B7),
+                      fontSize: 12,
+                    ),
+                    floatingLabelBehavior: FloatingLabelBehavior.auto,
+                    border: InputBorder.none,
+                    counterText: '',
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                  ),
+                  initialCountryCode: 'US',
+                  onChanged: (phone) {
+                    controller.phoneController.text = phone.completeNumber;
+                  },
+                ),
               ),
+
               const SizedBox(height: 30),
 
               // Continue button
